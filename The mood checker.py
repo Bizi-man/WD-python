275222006d0b7ca5c914e6f7fd35179e62f912ea
Igor Bizaj <rogi.jazib@gmail.com>
2023-05-17 21:04:19 +0200

    Brace yourself
    Homework is coming

---
+++ The mood checker.py
@@ -0,0 +1,21 @@
+print("What is your mood?")
+
+mood = "relaxed"
+
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+
+elif mood == "sad":
+    print("Who hurt you?")
+
+elif mood == "excited":
+    print("Lets go on an adventure")
+
+elif mood == "relaxed":
+    print("Nice do you want a beer?")
+
+else:
+    print("I dont recognise this mood.")
